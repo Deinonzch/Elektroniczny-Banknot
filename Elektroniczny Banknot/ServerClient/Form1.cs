a70840be454515c76ac6f7b98f76845b7e8a06b8
Deinonzch <slagol@st.amu.edu.pl>
2016-11-05 16:52:50 +0100

    Start
    Project Start

---
+++ Elektroniczny Banknot/ServerClient/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace ServerClient
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string receive;
+        public string text_to_send;
+        public string message;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());      //get my own IP
+            foreach (IPAddress address in localIP)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    textBox4.Text = address.ToString();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)      //start server
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(textBox3.Text));
+            listener.Start();
+            textBox2.AppendText("The server is running at port:" + textBox3.Text + "\n");
+            client = listener.AcceptTcpClient();
+            STR = new StreamReader(client.GetStream());
+            STW = new StreamWriter(client.GetStream());
+            STW.AutoFlush = true;
+
+            backgroundWorker1.RunWorkerAsync(); //start receiving data
+            backgroundWorker2.WorkerSupportsCancellation = true; //Ability to cancel this thread
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            IPEndPoint IP_End = new IPEndPoint(IPAddress.Parse(textBox5.Text), int.Parse(textBox6.Text));
+
+            try
+            {
+                client.Connect(IP_End);
+                if(client.Connected)
+                {
+                    textBox2.AppendText("Connected to server" + "\n");
+                    STW = new StreamWriter(client.GetStream());
+                    STR = new StreamReader(client.GetStream());
+                    STW.AutoFlush = true;
+
+                    backgroundWorker1.RunWorkerAsync(); //start receiving data
+                    backgroundWorker2.WorkerSupportsCancellation = true; //Ability to cancel this thread
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //Send button
+        {
+            if(textBox1.Text != "")
+            {
+                text_to_send = textBox1.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }           
+            textBox1.Text = "";
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) //receive data
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    receive = STR.ReadLine();
+                    this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("Get: " + receive + "\n"); }));
+
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message.ToString());
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e) //send data
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(text_to_send);
+                this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("Send: " + text_to_send + "\n"); }));
+            }
+            else
+            {
+                MessageBox.Show("Send failed!");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+    }
+}
